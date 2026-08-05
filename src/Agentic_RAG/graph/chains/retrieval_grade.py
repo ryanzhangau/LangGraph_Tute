69e289c16107f41ca048b24fc197cc7b8cfc675f
ryanzhangau <ryanzhangau@gmail.com>
2026-08-05 17:02:02 +1000

    refactor: update GraderDocument class name

--- src/Agentic_RAG/graph/chains/retrieval_grade.py
+++ src/Agentic_RAG/graph/chains/retrieval_grade.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel, Field
+
+load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash",
@@ -8,13 +11,13 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-class Document_Grader(BaseModel):
+class GradeDocuments(BaseModel):
     """Binary score for relevance check on retrieved documents."""
 
     binary_score: str = Field(description="Documents are relevant to the question, 'yes' or 'no'")
 
 
-structured_llm_grader = llm.with_structured_output(Document_Grader)
+structured_llm_grader = llm.with_structured_output(GradeDocuments)
 
 system = """You are a grader assessing relevance of a retrieved document to a user question. \n
 If the document contains keyword(s)  or semantic meaning related to the question, grade it as relevant document.
