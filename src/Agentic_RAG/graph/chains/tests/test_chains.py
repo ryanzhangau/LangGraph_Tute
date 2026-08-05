6100edd3cbaaf5ce37539011447ffcdf72046faa
ryanzhangau <ryanzhangau@gmail.com>
2026-08-05 17:02:38 +1000

    test: create unit tests

--- src/Agentic_RAG/graph/chains/tests/test_chains.py
+++ src/Agentic_RAG/graph/chains/tests/test_chains.py
@@ -1,2 +1,39 @@
-def test_foo() -> None:
-    assert 1 == 1
+from langchain_core.documents import Document
+
+from Agentic_RAG.graph.chains.retrieval_grade import GradeDocuments, grade_chain
+from Agentic_RAG.ingestion import chroma_retriever
+
+
+def test_retrieve_grader_answer_yes() -> None:
+    chroma_retriever.vectorstore.add_documents([
+        Document(page_content="This is mock document one text content."),
+        Document(page_content="This is mock document two text content.")
+    ])
+    query = "mock"
+    docs = chroma_retriever.invoke(query)
+    doc_text = docs[0].page_content
+
+    res: GradeDocuments = grade_chain.invoke({
+        "question": query,
+        "document": doc_text
+    })
+
+    assert res.binary_score == "yes"
+
+
+
+def test_retrieve_grader_answer_no() -> None:
+    chroma_retriever.vectorstore.add_documents([
+        Document(page_content="This is mock document one text content."),
+        Document(page_content="This is mock document two text content.")
+    ])
+    query = "Other questions"
+    docs = chroma_retriever.invoke(query)
+    doc_text = docs[0].page_content
+
+    res: GradeDocuments = grade_chain.invoke({
+        "question": query,
+        "document": doc_text
+    })
+
+    assert res.binary_score == "no"
--- src/Agentic_RAG/graph/nodes/retrieve.py
+++ src/Agentic_RAG/graph/nodes/retrieve.py
@@ -1,9 +1,10 @@
 from typing import Any, Dict
 
 from Agentic_RAG.graph.state import GraphState
-from Agentic_RAG.ingestion import retriever
+from Agentic_RAG.ingestion import chroma_retriever
 
 
 def retrieve_node(state: GraphState) -> Dict[str, Any]:
-    docs = retriever.invoke(state["question"])
+    docs = chroma_retriever.invoke(state["question"])
     return {"documents": docs, "question": state["question"]}
+
